59650320b798e603e0f240d4cde41fbeb1f19516
Axltz <axelmtz9880.com>
2024-09-04 20:55:05 -0600

    Agregue el archivo de inventario base, declare las listas principales y creé la función para mostrar el inventario.

--- InventarioTienda.py
+++ InventarioTienda.py
@@ -2,15 +2,12 @@
 import os
 
 Lista_Productos = []
-Nombres =  [1,2,3]
-Precios = [1,2,3]
-Stock = [1,2,3]
-Lista_Productos.append(Nombres)
-Lista_Productos.append(Precios)
-Lista_Productos.append(Stock)
+Nombres =  []
+Precios = []
+Stock = []
+
 def Mostrar_Productos():
     print("Listado de productos")
     for i, productos in enumerate(Lista_Productos, start=1):
         Nombres, Precios, Stock = productos
         print(f"{i}. {Nombres} - Precio por unidad: ${Precios:.2f}, Cantidad: {Stock} unidades")
-Mostrar_Productos()
