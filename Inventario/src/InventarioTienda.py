2c0d590f9125fbc4c6e797b9dd7f9af39366e720
Axltz <axelmtz9880.com>
2024-09-18 19:52:15 -0600

    Se modificaron funciones para mejorar rendimiento y lógica, se agregaron todas las funciones para los proveedores y se arreglaron funciones que no servian.

--- Inventario/src/InventarioTienda.py
+++ Inventario/src/InventarioTienda.py
@@ -2,7 +2,8 @@
 import os
 
 Lista_Productos = []
-Ventas_uni  = []
+Proveedores = []
+Ventas  = []
 Ingresos_totales = 0
 #módulo de menus
 def Mostrar_Main_Menu():
@@ -12,7 +13,7 @@ def Mostrar_Main_Menu():
     print("***************************")
 
     print("Bienvenido, seleccione una opción por favor.")
-    print("\n\t1. Gestor del inventario.\n\t2. Visor del inventario\n\t3. Ventas\n\t0. Salir del programa")
+    print("\n\t1. Gestor del inventario.\n\t2. Visor del inventario\n\t3. Gestor de proveedores\n\t4. Ventas\n\t0. Salir del programa")
     while True:
       seleccion = input("Elección: ")
       if seleccion == "1":
@@ -25,7 +26,12 @@ def Mostrar_Main_Menu():
          break
       if seleccion == "3":
          os.system('cls' if os.name == 'nt' else 'clear')
+         Menu_proveedores()
          Menu_ventas()
+         break
+      if seleccion == "4":
+         os.system('cls' if os.name == 'nt' else 'clear')
+         Menu_ventas()         
          break
          
       elif seleccion == "0":
@@ -72,22 +78,22 @@ def Menu_Inventario():
           os.system('cls' if os.name == 'nt' else 'clear')
           while True:
             Mostrar_Productos()
-            if confirmar_salida("¿Deseas salir?: "):
+            if confirmar_salida("¿Deseas salir? (si/no): "):
                print("Saliendo...")
                break
             else: os.system('cls' if os.name == 'nt' else 'clear')
           os.system('cls' if os.name == 'nt' else 'clear')
-      if seleccion == "2":
+      elif seleccion == "2":
           while True:
             os.system('cls' if os.name == 'nt' else 'clear')
             Buscar_Producto()
-            if confirmar_salida("¿Deseas salir?: "):
+            if confirmar_salida("¿Deseas salir? (si/no): "):
                print("Saliendo...")
                os.system('cls' if os.name == 'nt' else 'clear')
                break
             else: os.system('cls' if os.name == 'nt' else 'clear')
             os.system('cls' if os.name == 'nt' else 'clear')
-      if seleccion == "3":
+      elif seleccion == "3":
           os.system('cls' if os.name == 'nt' else 'clear')
           while True:
             Low_Stock(umbral = 5)
@@ -96,16 +102,25 @@ def Menu_Inventario():
                break
             else: os.system("cls")
             os.system('cls' if os.name == 'nt' else 'clear')
-      if seleccion == "4":
+      elif seleccion == "4":
           os.system('cls' if os.name == 'nt' else 'clear')
           while True:
             Reabastecer_Producto()
             if confirmar_salida("¿Deseas salir? (si/no): "):
                print("Saliendo...")
                break
-            else: os.system("cls")
+            else: os.system('cls' if os.name == 'nt' else 'clear')
             os.system('cls' if os.name == 'nt' else 'clear')
-      if seleccion == "0":
+      elif seleccion == "5":
+          os.system('cls' if os.name == 'nt' else 'clear')
+          while True:
+            Estadisticas_Stock()
+            if confirmar_salida("¿Deseas salir? (si/no): "):
+               print("Saliendo...")
+               break
+            else: os.system('cls' if os.name == 'nt' else 'clear')
+            os.system('cls' if os.name == 'nt' else 'clear')
+      elif seleccion == "0":
           if confirmar_salida("¿Deseas salir del visor del inventario? (si/no): "):
              print("Saliendo...")
              break
@@ -131,6 +146,47 @@ def Menu_ventas():
              break
           else: os.system('cls' if os.name == 'nt' else 'clear')
           os.system('cls' if os.name == 'nt' else 'clear')
+def Menu_proveedores():
+   while True:
+      print("Has ingresado al gestor de proveedores.\n")
+      print("Por favor seleccione una opción:")
+      print("\n\t1. Mostrar Proveedores\n\t2. Agregar proveedor.\n\t3. Editar proveedor.\n\t4. Eliminar proveedor.\n\t5. Historial de pedidos.\n\t6. Ordenes\n\t0. Salir\n")
+      seleccion = input("Elección: ")
+      if seleccion == "1":
+         os.system('cls' if os.name == 'nt' else 'clear')
+         while True:
+            Mostrar_Proveedores()
+            if confirmar_salida("¿Deseas salir? (si/no): "):
+               print("Saliendo...")
+               break
+            else: os.system('cls' if os.name == 'nt' else 'clear')
+         os.system('cls' if os.name == 'nt' else 'clear')
+      elif seleccion == "2":
+         os.system('cls' if os.name == 'nt' else 'clear')
+         Agregar_Proveedor()
+         os.system('cls' if os.name == 'nt' else 'clear')
+      elif seleccion == "3":
+         os.system('cls' if os.name == 'nt' else 'clear')
+         Editar_Proveedor()
+         os.system('cls' if os.name == 'nt' else 'clear')
+      elif seleccion == "4":
+         os.system('cls' if os.name == 'nt' else 'clear')
+         Eliminar_Proveedor()
+         os.system('cls' if os.name == 'nt' else 'clear')
+      elif seleccion == "5":
+         os.system('cls' if os.name == 'nt' else 'clear')
+         Historial_Pedidos()
+         os.system('cls' if os.name == 'nt' else 'clear')
+      elif seleccion == "6":
+         os.system('cls' if os.name == 'nt' else 'clear')
+         Control_Ordenes()
+         os.system('cls' if os.name == 'nt' else 'clear')
+      elif seleccion == "0":  
+         if confirmar_salida("¿Estás seguro de que deseas salir del gestor? (si/no): "):
+            print("Saliendo de la opción...")
+            os.system('cls' if os.name == 'nt' else 'clear')
+            break
+         os.system('cls' if os.name == 'nt' else 'clear')
 
 #Módulo de edición de productos         
 def Agregar_Producto():    
@@ -167,20 +223,20 @@ def Editar_Producto():
            break
         Mostrar_Productos()
         
-        seleccion = validar_seleccion_producto("¿Qué producto deseas editar? ", Lista_Productos)
+        seleccion = validar_seleccion("¿Qué producto deseas editar? ", Lista_Productos)
         producto = Lista_Productos[seleccion]  
         
         print("¿Qué deseas hacer? \n")
         print("\t1. Actualizar nombre.\n\t2. Actualizar stock.\n\t3. Actualizar precio.")
-        accion = int(input("Acción: "))
+        seleccion = int(input("seleccion: "))
         
-        if accion == 1: 
+        if seleccion == 1: 
             producto['nombre'] = input("Ingresa el nuevo nombre del producto: ")
             print("Nombre actualizado exitosamente!")
-        elif accion == 2:
+        elif seleccion == 2:
             producto['stock'] = int(input("Ingresa el stock disponible: ")) 
             print("Stock actualizado exitosamente!")
-        elif accion == 3:
+        elif seleccion == 3:
             producto['precio'] = float(input("Ingresa el precio nuevo: ")) 
             print("Precio actualizado exitosamente!")
         else:
@@ -197,9 +253,8 @@ def Editar_Producto():
 def Eliminar_Producto():
   while True:
     print("Has seleccionado eliminar producto")
-    print("¿Qué producto deseas eliminar?")
     Mostrar_Productos()
-    seleccion = validar_seleccion_producto("¿Qué producto deseas eliminar? ", Lista_Productos)
+    seleccion = validar_seleccion("¿Qué producto deseas eliminar? ", Lista_Productos)
 
     if confirmar_accion(f"¿Estas seguro de eliminar el producto '{Lista_Productos[seleccion]['nombre']}'? (si/no): "):
       del Lista_Productos[seleccion]
@@ -258,14 +313,113 @@ def Reabastecer_Producto():
          stock = int(input("Ingresa la cantidad de stock a agregar: "))
          re_stock(producto, stock) 
          if not confirmar_accion("¿Deseas reabastecer otro producto? (si/no): "):
+            os.system('cls' if os.name == 'nt' else 'clear')
             break
      else:
          print("Producto no encontrado.")
          if confirmar_salida("¿Deseas salir de la opción? (si/no): "):
+          os.system('cls' if os.name == 'nt' else 'clear')
           break
 def re_stock(producto, stock): 
     producto['stock'] += stock  
     print(f"Stock del producto actualizado, nuevo stock: {producto['stock']}")
+def Estadisticas_Stock():
+   while True:
+     total_productos = len(Lista_Productos)
+     total_stock = sum(producto['stock'] for producto in Lista_Productos)
+     print(f"Total de productos: {total_productos}")
+     print(f"Total de stock: {total_stock}")
+     break
+#módulo de proveedores
+def Mostrar_Proveedores():
+    if not Proveedores:
+      print("No hay proveedores registrados!")
+    else:
+      print("Listado de proveedores")
+      for i, proveedor in enumerate(Proveedores, start=1):
+        print(f"{i}. Nombre del proveedor: {proveedor['nombre']} - telefono: {proveedor['contacto']} - producto que ofrece: {proveedor['producto']}.") 
+def Agregar_Proveedor():
+   while True:
+    print("Has seleccionado agregar un proveedor.\n")
+    Mostrar_Proveedores()
+    nombre_proveedor = input("Ingresa el nombre del proveedor: ")  
+    telefono = input("Ingresa el número de contacto del proveedor: ")
+    producto = input("¿Qué producto ofrece el proveedor: ")                     
+          
+    dic_proveedores = {
+      "nombre": nombre_proveedor,
+      "contacto": telefono,
+      "producto": producto} 
+            
+    Proveedores.append(dic_proveedores)
+    print("Actualizando inventario...\n")
+    print(f"proveedor '{dic_proveedores['nombre']}' fue agregado exitosamente.")
+
+        
+    print("Esta es la lista de proveedores actualizada: ")
+    Mostrar_Productos()
+          
+    if not confirmar_accion("¿Deseas agregar algun otro proveedor? (si/no): "):
+        os.system("cls")
+        break
+   os.system('cls' if os.name == 'nt' else 'clear')
+def Editar_Proveedor():
+   while True:
+      print("Has seleccionado editar proveedores. ")     
+      print("¿Qué producto deseas editar?")
+      if not Proveedores:
+          print("Error, no hay proveedores registrados, saliendo de la opción...")
+          break
+      Mostrar_Proveedores()
+        
+      seleccion = validar_seleccion("¿Qué proveedor deseas editar? ", Proveedores)
+      proveedor = Proveedores[seleccion]  
+        
+      print("¿Qué deseas hacer? \n")
+      print("\t1. Actualizar nombre.\n\t2. Actualizar telefono.\n\t3. Actualizar producto que ofrece.")
+      seleccion = int(input("seleccion: "))
+        
+      if seleccion == 1: 
+          proveedor['nombre'] = input("Ingresa el nuevo nombre del proveedor: ")
+          print("Nombre actualizado exitosamente!")
+      elif seleccion == 2:
+          proveedor['contacto'] = int(input("Ingresa el nuevo número de telefono: ")) 
+          print("Teléfono actualizado exitosamente!")
+      elif seleccion == 3:
+          proveedor['producto'] =input("Ingresa el producto que ofrece: ")
+          print("Producto ofertado actualizado exitosamente!")
+      else:
+          print("Opción no válida.")
+          continue
+
+      print("Actualizando inventario...\n")
+      print(f"Proveedor '{proveedor['nombre']}' fue editado exitosamente.")     
+      print("Esta es la lista de proveedores actualizada: ")
+      Mostrar_Proveedores()
+      if not confirmar_accion("¿Deseas editar algún otro proveedor? "):
+          break
+      os.system('cls' if os.name == 'nt' else 'clear')
+def Eliminar_Proveedor():
+  while True:
+    print("Has seleccionado eliminar proveedor")
+    Mostrar_Proveedores()
+    seleccion = validar_seleccion("¿Qué proveedor deseas eliminar? ", Proveedores)
+
+    if confirmar_accion(f"¿Estas seguro de eliminar el proveedor '{Proveedores[seleccion]['nombre']}'? (si/no): "):
+      del Proveedores[seleccion]
+      print("Eliminando proveedor...")
+      print("Aplicando cambios..")
+      print("Proveedor eliminado correctamente!")
+      print("Lista actualizada:\n")
+      if not confirmar_accion("¿Deseas eliminar algún otro proveedor? "):
+        break
+    else: 
+      print("Has seleccionado no eliminar el proveedor.") 
+      if not confirmar_accion("¿Deseas eliminar algún otro proveedor? (si/no)"):
+        print("Saliendo...")
+        break
+      Mostrar_Proveedores()     
+    os.system('cls' if os.name == 'nt' else 'clear')
 
 #módulo de ventas y estadisticas
 def Venta():
@@ -304,7 +458,7 @@ def Hacer_venta(nombre, stock):
                         'precio_unitario': producto['precio'],
                         'precio_total' : total_venta
                     }
-                    Ventas_uni.append(ventas_dicc) 
+                    Ventas.append(ventas_dicc) 
                     print(f"Has vendido {stock} unidades de '{producto['nombre']}'.")
                     Ingresos_totales += total_venta  
                 else:
@@ -315,11 +469,11 @@ def Hacer_venta(nombre, stock):
 def Reporte_Ventas():
    while True:
      print("Has seleccionado ver el reporte de ventas.")
-     if not Ventas_uni:
+     if not Ventas:
         print("No hay ventas aún!")
      else:
         print("El historial de ventas es el siguiente:\n")
-        for i, producto in enumerate(Ventas_uni, start=1):
+        for i, producto in enumerate(Ventas, start=1):
           print(f"{i}. {producto['Nombre_del_producto']} - Precio por unidad: ${producto['precio_unitario']:.2f}, Cantidades vendidas: {producto['cantidad_vendida']} unidades, total de venta: ${producto['precio_total']:.2f}") 
      print(f"Los ingresos totales son: ${Ingresos_totales:.2f}")
      if confirmar_salida("¿Deseas salir de la opción? (si/no): "):
@@ -353,7 +507,7 @@ def validar_stock():
                 print("El stock debe ser mayor a 0.")  
              except ValueError:
                 print("Debes ingresar un número válido.") 
-def validar_seleccion_producto(mensaje, lista):
+def validar_seleccion(mensaje, lista):
     while True:
         try:
             seleccion = int(input(mensaje)) - 1
@@ -386,10 +540,13 @@ if __name__ == "__main__":
    Mostrar_Main_Menu()
 
 #funciones restantes   
-def Estadisticas_Stock():
-   print()
 
-def Descuentos():
+
+def aplicar_descuento():
    print()
 def Ordenar_productos():
+   print()
+def Historial_Pedidos():
+   print()
+def Control_Ordenes():
    print()
